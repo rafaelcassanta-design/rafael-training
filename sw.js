8f3abe51b93e8df3a551b3e405ad7b79fda034dc
rafaelcassanta-design <rafael.cassanta@gmail.com>
2026-09-05 12:46:34 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rafael-training-beta-v1-5';
+const CACHE='rafael-training-beta-v1-6';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})()));
